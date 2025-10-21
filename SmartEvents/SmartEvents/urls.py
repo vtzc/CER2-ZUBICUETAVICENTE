47e16300a65fe14acc03a0aca9cb37fa159b5420
vtzc <vzubicueta@usm.cl>
2025-10-21 11:12:00 -0300

    v2

--- SmartEvents/SmartEvents/urls.py
+++ SmartEvents/SmartEvents/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
+    path('eventos/', views.eventos, name="eventos"),
+    path('comunidad/', views.comunidad, name="comunidad"),
     path('admin/', admin.site.urls),
 ]
